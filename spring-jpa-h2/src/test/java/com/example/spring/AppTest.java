92c77a9f48d71f5331aea510654f5139c6318839
MohamedElBoutahiri <elamarti.med@gmail.com>
2019-05-18 23:00:02 +0200

    Add test to have the structure src/main; src/test.

---
+++ spring-jpa-h2/src/test/java/com/example/spring/AppTest.java
@@ -0,0 +1,15 @@
+package com.example.spring;
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
