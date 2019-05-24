3d47cf10ac97b1c51b679460ba3cbc5299d5019d
MohamedElBoutahiri <elamarti.med@gmail.com>
2019-05-24 20:53:10 +0200

    Creation of the home controller with some tests

---
+++ shoes-cloud-springboot/src/main/java/com/shoes/cloud/ShoesCloudApplication.java
@@ -0,0 +1,13 @@
+package com.shoes.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesCloudApplication.class);
+	}
+
+}
