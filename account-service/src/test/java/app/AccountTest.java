6ebe71e8750e9baf3715f42f3a1e06d174353e00
MohamedElBoutahiri <elamarti.med@gmail.com>
2019-06-14 20:30:14 +0200

    Delete of unused imports

--- account-service/src/test/java/app/AccountTest.java
+++ account-service/src/test/java/app/AccountTest.java
@@ -3,7 +3,6 @@ package app;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
